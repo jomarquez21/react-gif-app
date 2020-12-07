d420d7fb3f5b33420b0bb7a993ac520a9b78d870
Jose Enrique Marquez <jmrqz21@gmail.com>
2020-12-07 14:46:48 -0500

    Add docs folder to github page

---
+++ docs/precache-manifest.4036d868f829742c4612caad7ddef4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883bd951c29178a32dab3c9404789896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe2d2e13f58f816c8ac",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "a4032336a1b1e0556811",
+    "url": "/static/js/2.1e512c44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e512c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe2d2e13f58f816c8ac",
+    "url": "/static/js/main.7667721d.chunk.js"
+  },
+  {
+    "revision": "c9a797bf627a9c2970ca",
+    "url": "/static/js/runtime-main.4c8a308c.js"
+  }
+]);
